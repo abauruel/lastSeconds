bac31e161ae8a05211749098cbc6f03e2ce5d747
Alex Claude <abauruel@gmail.com>
2024-11-23 13:31:57 -0300

    feat? add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from gpio_test import button_pressed_callback
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     with app.app_context():
         button_pressed_callback(8)
     # Register API routes
